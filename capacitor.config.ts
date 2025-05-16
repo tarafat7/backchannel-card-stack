51e19f2d843ae655a67cbdfb75cac981b24b97ac
frank-besson <frankbesson@outlook.com>
2025-05-16 16:02:22 -0400

    nice

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shelf.backchannel',
+  appName: 'backchannel',
+  webDir: 'dist'
+};
+
+export default config;
